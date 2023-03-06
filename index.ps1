0ba39262d937f28754144d90850b4a694c33d07c
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2023-03-06 09:51:08 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -42,14 +42,24 @@ function Add-LocalGroup {
 ########################## Distribution ############################
 ####################################################################
 # installs software and configurs them from a configuration file
-# callable: InstallFrom("C:\test\installation.json")
-function InstallFrom {
+# callable: Install("C:\test\installation.json")
+function Install {
   param(
     [string[]] $InstallationFile
   )
   
   Write-Host "${InstallationFile} is set as installation source..."
   # parse JSONfile with installation metadata from here on
+  $Applications = Get-Content -Path $InstallationFile | ConvertFrom-Json 
+  
+  # output the applications it found
+  Write-Host "Found the following applications to install: "
+  $Applications | Format-Table
+  
+  # Testing JSON Objects
+  <#foreach ($application in $Applications) {
+   # write-host "$($application.ApplicationName) has the binaryLocation: $($application.BinaryLocation) and the flags: $($application.InstallFlags)"
+  }#>
 }
 
 
