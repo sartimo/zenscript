a1dc1c69d976ee1f8e9d4cba40e5fe4e1cdce290
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2023-03-06 09:54:03 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,1 +1,6 @@
 Import-Module "./module.psm1"
+
+Rename-Machine("PC-BOB")
+Join-Domain("conto.so". "OU=testOU,DC=domain,DC=Domain,DC=com")
+Add-LocalGroup("Administrators", "conto.so\timo.sarkar")
+Install("C:\test\installation.json")
