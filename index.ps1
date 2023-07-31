80b881a04322bc1fe86e99531ca66b9418df807b
Timo Sarkar <71646577+0xtimosarkar@users.noreply.github.com>
2023-07-31 15:41:20 +0200

    Delete index.ps1

--- index.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module ".\module\module.psm1"
-
-Rename-Machine("PC-BOB")
-Join-Domain("conto.so". "OU=testOU,DC=domain,DC=Domain,DC=com")
-Add-LocalGroup("Administrators", "conto.so\timo.sarkar")
-Install(".\configuration\installation.json")
